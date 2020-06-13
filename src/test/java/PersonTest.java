4ee5967f8f4a732da792468d20cbfd5cac58fb04
eric <purum@postech.ac.kr>
2020-06-13 19:16:02 +0900

    Person test

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -3,7 +3,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.dropwizard.jackson.Jackson;
 import io.dropwizard.micro.Person;
 
-import org.junit.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PersonTest {
@@ -17,8 +18,15 @@ public class PersonTest {
         final String expected = MAPPER.writeValueAsString(
                 MAPPER.readValue(fixture("fixtures/person.json"), Person.class));
         
-        System.out.println(expected);
-
         assertThat(MAPPER.writeValueAsString(person)).isEqualTo(expected);
+    }
+    
+    @Test
+    public void deserializesFromJSON() throws Exception {
+        final Person person = new Person("Luther Blissett", "lb@example.com");
+        final Person expected = MAPPER.readValue(fixture("fixtures/person.json"), Person.class);
+
+        assertThat(expected.getName()).isEqualTo(person.getName());
+        assertThat(expected.getEmail()).isEqualTo(person.getEmail());
     }
 }
